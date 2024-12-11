0096a1cf60a263de7abbe8d91b309181e92aa6bf
Snehasish Chakraborty <114snehasish@gmail.com>
2024-12-12 01:13:49 +0530

    Add Main component and adjust Header component structure
    Introduce a new `Main` component with its corresponding CSS for layout and styling. Update the file structure of the `Header` component to follow consistent naming conventions. Ensure the `App` integrates the new `Main` component alongside the updated `Header`.

---
+++ src/components/main/Main.tsx
@@ -0,0 +1,17 @@
+import './main.css';
+
+const Main = () => {
+  return (
+    <main className="main">
+      <section className="main-section">
+        <h1>Lorem ipsum dolor sit.</h1>
+        <h1>Lorem ipsum dolor sit amet.</h1>
+        <h1>Lorem ipsum dolor sit amet.</h1>
+        <h1>Lorem ipsum dolor sit amet.</h1>
+        <h1>Lorem ipsum dolor sit amet.</h1>
+      </section>
+    </main>
+  );
+};
+
+export default Main;
