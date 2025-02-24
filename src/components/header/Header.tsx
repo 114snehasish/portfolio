38b928d9cda37604a46a478a481b11af429ffb33
Snehasish Chakraborty <114snehasish@gmail.com>
2025-02-25 02:29:15 +0530

    Add react-icons and implement icons in header navigation
    Integrate `react-icons` library into the project and update `NavItem` structure to include an `icon` property. Modify `Header` component to display icons in mobile navigation and adjust the CSS for responsive design. Refactor navigation styles for better flexibility and consistency across screen sizes.

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,25 +1,49 @@
 import './header.css';
+import { FaAddressCard } from 'react-icons/fa6';
+import { IconType } from 'react-icons';
 
 interface NavItem {
   href: string;
   label: string;
   type: 'link' | 'button';
+  icon: IconType;
 }
 export default function Header() {
   const navItems: NavItem[] = [
-    { href: '#about', label: '_about', type: 'link' },
-    { href: '#skills', label: '_skills', type: 'link' },
-    { href: '#experiences', label: '_experiences', type: 'link' },
-    { href: '#article', label: '_articles', type: 'link' },
-    { href: '#contact', label: '_contact', type: 'link' },
-    { href: '/resume.pdf', label: '/resume', type: 'button' },
+    { href: '#about', label: '_about', type: 'link', icon: FaAddressCard },
+    { href: '#skills', label: '_skills', type: 'link', icon: FaAddressCard },
+    {
+      href: '#experiences',
+      label: '_experiences',
+      type: 'link',
+      icon: FaAddressCard,
+    },
+    { href: '#article', label: '_articles', type: 'link', icon: FaAddressCard },
+    { href: '#contact', label: '_contact', type: 'link', icon: FaAddressCard },
+    {
+      href: '/resume.pdf',
+      label: '/resume',
+      type: 'button',
+      icon: FaAddressCard,
+    },
   ];
   const renderLink = (item: NavItem, index: number) => {
     return (
-      <a className="header-text" key={item.href} href={item.href}>
-        <span>{index + 1}. </span>
-        <span>{item.label}</span>
-      </a>
+      <>
+        <a className="header-text header-text-desktop">
+          <span>{index + 1}. </span>
+          <span>{item.label}</span>
+        </a>
+        <a
+          className="header-text header-text-mobile"
+          key={item.href}
+          href={item.href}
+        >
+          <span>
+            <item.icon />
+          </span>
+        </a>
+      </>
     );
   };
   const renderButton = (item: NavItem) => {
@@ -36,7 +60,7 @@ export default function Header() {
 
   return (
     <header className="p-8">
-      <nav className="flex justify-end items-center space-x-8">
+      <nav className="nav">
         {navItems.map((item, index) =>
           item.type === 'link' ? renderLink(item, index) : renderButton(item)
         )}
