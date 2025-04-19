950cf9f5785d33a0e130e981b7de774715173e17
Snehasish Chakraborty <114snehasish@gmail.com>
2025-04-20 01:57:03 +0530

    Enhance About and Main components with new styling and content
    - Introduced new bullet-point layout in the About component for improved content presentation.
    - Added Tailwind component classes for consistent text styling across components.
    - Updated Main component to utilize the new text-regular class for better readability.
    - Adjusted CSS in various components for enhanced layout and responsiveness.

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -9,14 +9,14 @@ const Main = () => {
     <main className="main">
       <section className="main-section">
         <div className="section-container">
-          <p>
+          <p className="text-regular">
             <span className="text-secondary">Hi,</span> I am
           </p>
           <div className="big-text">
             <h1>Snehasish Chakraborty.</h1>
             <h1 className="caption">I help team build products for the web.</h1>
           </div>
-          <p className="self-description">
+          <p className="self-description text-regular">
             I am a software engineer having{' '}
             <span className="text-secondary">{yearsOfExperience}</span> years of
             experience specializing in{' '}
