7c690e62df2fdbc2222a33b9f95467d4eb5e0962
Snehasish Chakraborty <114snehasish@gmail.com>
2025-08-27 01:47:02 +0530

    Fix import statement for image optimization plugin by removing file extension

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { imageOptimization } from './plugins/image-optimization.js';
+import { imageOptimization } from './plugins/image-optimization';
 
 // https://vitejs.dev/config/
 export default defineConfig({
